0e72ad2dfbddef79ea5eeaf5b54aaa170520fd6a
nghoangviet171@gmail.com <nghoangviet17@gmail.com>
2020-09-17 08:04:36 +0700

    viet pushed code to new git

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express';
- 
+import passport from 'passport';
+const GoogleStratergy = require('passport-google-oauth20').Stratergy;
 const app = express();
  
-app.get('/', (req, res) => {
-  res.send('byeeeeeeeeee buddy');
-});
+passport.use(new GoogleStratergy());
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
